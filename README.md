046710633c2163b6bf6e53adead7df6522e41e12
Majdi-msallem <majdi.msallem@esprit.tn>
2024-05-02 16:11:42 +0200

    migrations

--- README.md
+++ README.md
@@ -1,68 +1,19 @@
 # README
 
 
-Active Record in Ruby on Rails
-Active Record is the Object-Relational Mapping (ORM) layer provided by Ruby on Rails. It allows you to interact with your database using Ruby objects, making database operations intuitive and efficient.
-
-Getting Started
-To start using Active Record in your Rails application, make sure you have defined your database configuration in config/database.yml. Rails uses this configuration to connect to your database.
-
-
-
-Voici un guide simple pour créer un modèle avec Rails:
-
----
-
-# Creating a Model with Rails
-
-In Ruby on Rails, models represent the data of your application and handle interactions with the database. Here's how you can create a model in Rails:
-
-## Step 1: Generate the Model
-
-Use the Rails generator to create a new model. For example, to create a `User` model with `name` and `email` attributes, run:
-
-```bash
-rails generate model User name:string email:string
-```
-
-This command will generate a migration file and a model file for the `User` model.
-
-## Step 2: Run the Migration
-
-Run the migration to create the corresponding database table for your model:
-
 ```bash
 rails db:migrate
 ```
 
-This will execute the migration file and create the `users` table with `name` and `email` columns in your database.
-
-## Step 3: Use the Model
-
-You can now use the `User` model in your Rails application. For example, to create a new user:
-
-```ruby
-user = User.new(name: "John", email: "john@example.com")
-user.save
+```bash
+-Rollback migration retourner a la derniere migration
+rails db:rollback
 ```
 
-Or to find a user by their ID:
+```bash
+-ajouter des nouveau attribut pour cree un fichier de migration
+rails g migration add_columns_to authors address:string contact:string
+-lancer la migration 
+rails db:migrate
 
-```ruby
-user = User.find(1)
 ```
-
-## Step 4: Customize the Model
-
-You can customize the model by adding validations, associations, and methods as needed. Here's an example of adding a validation to ensure the presence of a name:
-
-```ruby
-class User < ApplicationRecord
-  validates :name, presence: true
-end
-```
-
-This ensures that every user record must have a name.
-
----
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_133431) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_02_140715) do
+  create_table "assemblies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "assemblies_parts", id: false, force: :cascade do |t|
+    t.integer "assembly_id", null: false
+    t.integer "part_id", null: false
+    t.index ["assembly_id", "part_id"], name: "index_assemblies_parts_on_assembly_id_and_part_id", unique: true
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "isbn"
+    t.integer "price"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "channel_subscriptions", force: :cascade do |t|
+    t.integer "subscriber_id", null: false
+    t.integer "video_channel_id", null: false
+    t.datetime "subscribed_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subscriber_id"], name: "index_channel_subscriptions_on_subscriber_id"
+    t.index ["video_channel_id"], name: "index_channel_subscriptions_on_video_channel_id"
+  end
+
+  create_table "parts", force: :cascade do |t|
+    t.string "name"
+    t.string "part_no"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "lastname"
@@ -19,4 +73,21 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_133431) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "subscribers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "video_channels", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "books", "authors"
+  add_foreign_key "channel_subscriptions", "subscribers"
+  add_foreign_key "channel_subscriptions", "video_channels"
 end
