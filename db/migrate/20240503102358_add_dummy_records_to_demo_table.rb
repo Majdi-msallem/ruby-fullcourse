7d72c82d3bb3f1476c149450ef0565acdd870a34
Majdi-msallem <majdi.msallem@esprit.tn>
2024-05-03 12:36:13 +0200

    migration-reversible

---
+++ db/migrate/20240503102358_add_dummy_records_to_demo_table.rb
@@ -0,0 +1,23 @@
+class AddDummyRecordsToDemoTable < ActiveRecord::Migration[7.1]
+  def change
+    dir.up do
+    10.times do |i|
+      puts "Creating Demo #{i+1}"
+      Demo.create(
+        name: "Demo #{i+1}",
+        description: "Demo description for Demo #{i+1}",
+      )
+    end
+    end
+
+    dir.down do
+      10.times do |i|
+        puts "Deleting Demo #{i+1}"
+          Demo.find_by(name: "Demo Name  #{i+1}").destroy
+
+      end
+    end
+
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_03_100953) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_03_102358) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "description"
